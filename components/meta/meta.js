c7cfdce9bb91fcf91fe80be7a1f764d578210e42
Ashwitha Thangarajan <139689176+robin11110000@users.noreply.github.com>
2025-01-30 18:41:17 +0530

    Update meta.js

--- components/meta/meta.js
+++ components/meta/meta.js
@@ -4,18 +4,14 @@ export default function Meta() {
     return (
         <>
         <Head>
-            <title>NFTPort | NFT Minting Starter Kit</title>
-            <meta name="description" content="NFTPort NFT Minting Starter Kit using NFTPort, Next.js, and Wagmi." />
+            <title>Unleash NFT | NFT Minting Starter Kit</title>
+            <meta name="description" content="Unleash NFT Minting Starter Kit using Unleash NFT, Next.js, and Wagmi." />
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />  
             <link rel="icon" href="/favicon.ico" />
-            <meta property="og:title" content="NFTPort | NFT Minting Starter Kit" />
+            <meta property="og:title" content="Unleash NFT | NFT Minting Starter Kit" />
             <meta property="og:type" content="article" />
-            <meta property="og:url" content="https://nftport-nft-minting-starter-kit.vercel.app/og.jpg" />
-            <meta property="og:image" content="https://nftport-nft-minting-starter-kit.vercel.app/og.jpg" />
-            <meta name="twitter:title" content="NFTPort | NFT Minting Starter Kit" />
-            <meta name="twitter:description" content="NFTPort NFT Minting Starter Kit using NFTPort, Next.js, and Wagmi." />
-            <meta name="twitter:image" content="https://nftport-nft-minting-starter-kit.vercel.app/og.jpg" />
-            <meta name="twitter:card" content="summary_large_image"></meta>
+            <meta property="og:url" content="https://your-unleash-nft-project-url/og.jpg" />
+            <meta property="og:image" content="https://your-unleash-nft-project-url/og.jpg" />
         </Head>
         </>
     )
